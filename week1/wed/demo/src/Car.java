bea6cb9ca847d02ed32945990cd6ba2d33da83f2
bduong0929 <bao.duong@revature.com>
2022-03-22 10:36:24 -0600

    added demo week1/wed

---
+++ week1/wed/demo/src/Car.java
@@ -0,0 +1,15 @@
+public class Car {
+    /* attributes */
+    String color;
+    String brand;
+    String model;
+
+    /* behaviors */
+    public void start() {
+        System.out.println("Engine starting...");
+    }
+
+    public void end() {
+        System.out.println("Engine stops...");
+    }
+}
